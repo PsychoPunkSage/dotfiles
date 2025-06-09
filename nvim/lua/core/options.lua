fa3fc29e49ebd43b3b03cc3b6793fd9b023806f8
Abhinav Prakash <abhinav.prakash319@gmail.com>
2025-06-09 23:14:18 +0530

    fix::> core utilities like key-mappings and options
    Signed-off-by: Abhinav Prakash <abhinav.prakash319@gmail.com>

---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,17 @@
+vim.wo.number = true -- Line numbers
+vim.o.relativenumber = true -- relative line numbers
+vim.o.clipboard = 'unnamedplus' -- Sync clipboard between OS and Neovim
+
+-- indenttaion
+vim.o.wrap = false -- Not allow long lines to exist
+vim.o.linebreak = true -- Prevent splitting words
+vim.o.autoindent = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true -- convert tabs to spaces
+
+vim.o.mouse = "a" -- Enable mouse mode
+vim.o.ignorecase = true -- Case-insensitive search || Unless \C or CAPS letters are involved
+vim.o.smartcase = true -- Smart Case
+
