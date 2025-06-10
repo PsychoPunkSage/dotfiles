8d71f9cca0026bb5d0bec83ac34ad2b810761089
Abhinav Prakash <abhinav.prakash319@gmail.com>
2025-06-10 23:23:57 +0530

    feat: Tokyo Night color theme added
    Signed-off-by: Abhinav Prakash <abhinav.prakash319@gmail.com>

--- nvim/lua/plugins/colortheme.lua
+++ nvim/lua/plugins/colortheme.lua
@@ -1,28 +1,62 @@
 return {
-    'shaunsingh/nord.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        -- Example config in lua
-        vim.g.nord_contrast = true
-        vim.g.nord_borders = false
-        vim.g.nord_disable_background = true
-        vim.g.nord_italic = false
-        vim.g.nord_uniform_diff_background = true
-        vim.g.nord_bold = false
+  -- 'shaunsingh/nord.nvim',
+  -- lazy = false,
+  -- priority = 1000,
+  -- config = function()
+  --     -- Example config in lua
+  --     vim.g.nord_contrast = true
+  --     vim.g.nord_borders = false
+  --     vim.g.nord_disable_background = true
+  --     vim.g.nord_italic = false
+  --     vim.g.nord_uniform_diff_background = true
+  --     vim.g.nord_bold = false
+  --
+  --     -- Load the colorscheme
+  --     require('nord').set()
 
-        -- Load the colorscheme
-        require('nord').set()
+  --
+  -- -- Toggle background transparency
+  -- local bg_transparent = true
+  --
+  -- local toggle_transparency = function()
+  --   bg_transparent = not bg_transparent
+  --   vim.g.nord_disable_background = bg_transparent
+  --   vim.cmd [[colorscheme nord]]
+  -- end
 
-        -- Toggle background transparency
-        local bg_transparent = true
+  'folke/tokyonight.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- Tokyo Night configuration
+    require('tokyonight').setup {
+      style = 'night', -- The theme comes in four styles, `storm`, `moon`, a darker variant `night` and `day`
+      transparent = false, -- Enable this to disable setting the background color
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+      styles = {
+        comments = { italic = false },
+        keywords = { italic = false },
+        functions = {},
+        variables = {},
+        sidebars = 'dark', -- style for sidebars, see below
+        floats = 'dark', -- style for floating windows
+      },
+    }
 
-        local toggle_transparency = function()
-          bg_transparent = not bg_transparent
-          vim.g.nord_disable_background = bg_transparent
-          vim.cmd [[colorscheme nord]]
-        end
+    -- Load the colorscheme
+    vim.cmd [[colorscheme tokyonight]]
 
-        vim.keymap.set('n', '<leader>bg', toggle_transparency, { noremap = true, silent = true })
+    -- Toggle background transparency
+    local bg_transparent = false
+
+    local toggle_transparency = function()
+      bg_transparent = not bg_transparent
+      require('tokyonight').setup {
+        transparent = bg_transparent,
+      }
+      vim.cmd [[colorscheme tokyonight]]
     end
+
+    vim.keymap.set('n', '<leader>bg', toggle_transparency, { noremap = true, silent = true })
+  end,
 }
