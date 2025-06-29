606ad214b09aa62e8c3d3913f3f2f6be7bfcc169
Abhinav Prakash <abhinav.prakash319@gmail.com>
2025-06-29 11:08:26 +0530

    fix::> configured DAP for Golang

--- nvim/lua/plugins/dap/dap.lua
+++ nvim/lua/plugins/dap/dap.lua
@@ -1,50 +1,3 @@
--- return {
---   'mfussenegger/nvim-dap',
---   config = function()
---     local dap, dapui = require 'dap', require 'dapui'
---     dap.listeners.before.attach.dapui_config = function()
---       dapui.open()
---     end
---     dap.listeners.before.launch.dapui_config = function()
---       dapui.open()
---     end
---     dap.listeners.before.event_terminated.dapui_config = function()
---       dapui.close()
---     end
---     dap.listeners.before.event_exited.dapui_config = function()
---       dapui.close()
---     end
---
---     -- DAP Keymaps
---     local map = vim.keymap.set
---
---     -- Core DAP controls
---     map('n', '<Leader>dl', function()
---       require('dap').step_into()
---     end, { desc = 'Debug: Step Into' })
---     map('n', '<Leader>dj', function()
---       require('dap').step_over()
---     end, { desc = 'Debug: Step Over' })
---     map('n', '<Leader>dk', function()
---       require('dap').step_out()
---     end, { desc = 'Debug: Step Out' })
---     map('n', '<Leader>dc', function()
---       require('dap').continue()
---     end, { desc = 'Debug: Continue' })
---     map('n', '<Leader>db', function()
---       require('dap').toggle_breakpoint()
---     end, { desc = 'Debug: Toggle Breakpoint' })
---     map('n', '<Leader>dd', function()
---       require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
---     end, { desc = 'Debug: Conditional Breakpoint' })
---     map('n', '<Leader>de', function()
---       require('dap').terminate()
---     end, { desc = 'Debug: Terminate' })
---     map('n', '<Leader>dr', function()
---       require('dap').run_last()
---     end, { desc = 'Debug: Run Last' })
---   end,
--- }
 return {
   'mfussenegger/nvim-dap',
   dependencies = {
@@ -63,6 +16,16 @@ return {
         -- Get CodeLLDB path from Mason
         command = vim.fn.stdpath 'data' .. '/mason/bin/codelldb',
         args = { '--port', '${port}' },
+      },
+    }
+
+    -- Configure Delve adapter for GoLang
+    dap.adapters.delve = {
+      type = 'server',
+      port = '${port}',
+      executable = {
+        command = vim.fn.stdpath 'data' .. '/mason/bin/dlv',
+        args = { 'dap', '-l', '127.0.0.1:${port}' },
       },
     }
 
@@ -92,6 +55,29 @@ return {
           local args_string = vim.fn.input 'Arguments: '
           return vim.split(args_string, ' ')
         end,
+      },
+    }
+
+    -- Configure Golang debugging
+    dap.configurations.go = {
+      {
+        type = 'delve',
+        name = 'Debug',
+        request = 'launch',
+        program = '${file}',
+      },
+      {
+        type = 'delve',
+        name = 'Debug Package',
+        request = 'launch',
+        program = '${fileDirname}',
+      },
+      {
+        type = 'delve',
+        name = 'Debug test',
+        request = 'launch',
+        mode = 'test',
+        program = '${file}',
       },
     }
 
